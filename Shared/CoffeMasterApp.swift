ce3f4e5f816bf8e1aa52f300f0be9460f4d40284
Igor V Costa <igor.vc30@gmail.com>
2022-10-16 23:46:35 -0300

    Initial Commit

---
+++ Shared/CoffeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMasterApp.swift
+//  Shared
+//
+//  Created by Igor Vasconcelos Costa on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
